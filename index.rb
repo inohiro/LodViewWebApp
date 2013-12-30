b1708937f5a0087938add9e3409dd1c0fb4f2f7e
Hiroyuki Inoue <mammymax@gmail.com>
2013-12-30 15:45:38 +0900

    fix for pushing to Heroku

--- index.rb
+++ index.rb
@@ -1,11 +1,23 @@
 require 'sinatra'
+require 'pp'
 
-require File.expand_path( "~/github/LodViewRewrite/lib/lod_view_rewrite.rb" )
+# require File.expand_path( "~/github/LodViewRewrite/lib/lod_view_rewrite.rb" )
 # require LodViewRewrite
 
 get '/' do
   # Description of API
   'Hello, World'
+end
+
+get '/test/:id/' do
+  view_id = params[:id]
+  query = request.query_string
+
+  3.times { puts "" }
+  puts "=================================================================="
+  puts "view_id: #{view_id}"
+  puts query
+  3.times { puts "" }
 end
 
 get '/views' do
@@ -26,14 +38,14 @@ end
 
 # API as LINQ Entrypoint
 
-get '/api/:view_id' do
-  condition = params[:condition]
-  view_id = params[:view_id]
+# get '/api/:view_id' do
+#   condition = params[:condition]
+#   view_id = params[:view_id]
 
-  view = Viea.find_by_id view_id
-  query = Query.new( view )
+#   view = Viea.find_by_id view_id
+#   query = Query.new( view )
 
-  filters = Filters.new( condition )
-  result = query.exec_sparql( filters )
-  return result
-end
+#   filters = Filters.new( condition )
+#   result = query.exec_sparql( filters )
+#   return result
+# end
