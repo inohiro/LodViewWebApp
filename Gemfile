e305f789e6f08e0bdfbe08a720b8b2ca81e1ae0f
Hiroyuki Inoue <mammymax@gmail.com>
2014-01-05 16:58:36 +0900

    temporary commit. confirmed request from LINQ Provider

--- Gemfile
+++ Gemfile
@@ -1,7 +1,18 @@
 source 'https://rubygems.org/'
 
 gem 'sinatra'
+gem 'sinatra-contrib'
+
 gem 'redis'
 gem 'foreman'
 
-gem 'newrelic_rpm'
+gem 'LodViewRewrite'
+
+group :test do
+  gem 'rspec'
+  gem 'rack-test'
+end
+
+group :production do
+  gem 'newrelic_rpm'
+end
--- index.rb
+++ index.rb
@@ -1,9 +1,12 @@
 # coding: utf-8
 
 require 'sinatra'
+require 'sinatra/json'
+
 require 'pp'
 require 'json'
 
+require 'LodViewRewrite'
 require 'newrelic_rpm'
 
 # require File.expand_path( "~/github/LodViewRewrite/lib/lod_view_rewrite.rb" )
@@ -11,6 +14,10 @@ require 'newrelic_rpm'
 
 # require File.expand_path( "../lib/query.rb" )
 # require File.expand_path( "../lib/filter.rb" )
+
+=begin
+
+=end
 
 require 'uri'
 
@@ -22,8 +29,11 @@ end
 get '/test/:id/' do
   view_id = params[:id]
   query = params["query"]
-  json = JSON.parse( URI.decode query )
-
+  begin
+    json = JSON.parse( URI.decode query )
+  rescue => ex
+    puts ex
+  end
   # filters = LodViewRewrite::Filters.new( query ).to_a
 
   "view_id: #{view_id}, query: #{json}"
@@ -47,9 +57,9 @@ end
 
 # API as LINQ Entrypoint
 
-# get '/api/:view_id' do
-#   condition = params[:condition]
-#   view_id = params[:view_id]
+get '/api/:view_id/' do
+  condition = params[:condition]
+  view_id = params[:view_id]
 
 #   view = Viea.find_by_id view_id
 #   query = Query.new( view )
@@ -57,4 +67,68 @@ end
 #   filters = Filters.new( condition )
 #   result = query.exec_sparql( filters )
 #   return result
-# end
+end
+
+def get_view( view_id )
+  queries = {
+    1 => "SELECT * WHERE { ?subject <http://dbpedia.org/property/prefecture> <http://dbpedia.org/resource/Tokyo> . }",
+  }
+
+  # queries[view_id]
+
+end
+
+get '/api/fixed/1/' do
+  view = test_view
+  query = LodViewRewrite::Query.new( view )
+  result = query.exec_sparql
+
+  content_type 'application/json'
+  result
+end
+
+get '/api/fixed/2/' do
+  view = test_view
+  query = LodViewRewrite::Query.new( view )
+  cond = [
+    {"Variable"=>"subject", "Condition"=>"http://dbpedia.org/resource/Minato,_Tokyo", "Operator"=>"=", "FilterType"=>'3',"ConditionType"=>"System.String"},
+    {"Variable"=>"subject", "AggregationType"=>'3'} # count of ?subject
+  ].to_json
+  condition = LodViewRewrite::Condition.new( cond )
+
+  result = query.exec_sparql( condition )
+
+  content_type 'application/json'
+  result
+  # json( {:result => result}, :encoder => :to_json, :content_type => :js )
+end
+
+get '/api/fixed/3/' do
+  view = test_view
+  query = LodViewRewrite::Query.new( view )
+
+  decoded = URI.decode( params['query'] )
+  query_condition = decoded.to_json
+  condition = LodViewRewrite::Condition.new( query_condition )
+
+  require 'pp'
+  puts '################################################'
+  pp condition
+
+  sparql = query.to_sparql( condition )
+  puts "SPARQL: #{sparql}"
+
+  result = query.exec_sparql( condition )
+  content_type 'application/json'
+  result
+end
+
+def test_view
+  query =<<EOQ
+SELECT *
+WHERE {
+  ?subject <http://dbpedia.org/property/prefecture> <http://dbpedia.org/resource/Tokyo> .
+}
+EOQ
+  query
+end
